8daf8a3053990e5aab6b2e5b430ee9da995bdeea
jlthaxto <106979967+jlthaxto@users.noreply.github.com>
2022-10-24 19:34:50 -0500

    building out app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+
+import React, { useState, useEffect } from 'react';
+// import ReactDOM from 'react-dom/client';
 import { Route, BrowserRouter, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 
@@ -12,8 +13,60 @@ import {
   Navbar
 } from './components'; 
 
-function App() {
+import {
+  getRoutines,
+  getUserDetails
+} from './api';
+
+
+const App = () => {
+  
+ const [routines, setRoutines] = useState([]);
+  const [token, setToken] = useState('');
+  const [user, setUser] = useState({});
+  
+  const navigate = useNavigate();
+  
+  function logout() {
+    window.localStorage.removeItem('token');
+    setToken('');
+    setUser({});
+  }
+  
+  async function fetchRoutines() {
+    const results = await getRoutines(token)
+    setRoutines(results.data.routines);
+  }
+  
+  async function getMe() {
+    const storedToken = window.localStorage.getItem('token');
+    
+    if (!token) {
+      if (storedToken) {
+        setToken(storedToken);
+      }
+      return;
+    }
+    
+    const results = await getUserDetails(token)
+    if (results.success) {
+      setUser(results.data);
+    } else {
+      console.log(results.error.message);
+    }
+  }
+  
+  useEffect(() => {
+    fetchRoutines();
+  }, [token])
+  
+  useEffect(() => {
+    getMe();
+  }, [token])
+  
   return (
+    <div>
+      <Navbar logout={ logout } token={ token }/>
     <BrowserRouter >
     <Routes>
 
@@ -31,6 +84,16 @@ function App() {
     />
 
 
+<Route 
+        path='/myRoutines' 
+        element={<myRoutines
+        //activities={activities} 
+    />} 
+    />
+
+
+    
+
     <Route 
       path='/Routines'
       element={<Routines
@@ -39,32 +102,33 @@ function App() {
     />
 
 
-<Route 
+{/* <Route 
       path='/Navbar'
       element={<Navbar
       //routines={routines}
     />}
-    />
+    /> */}
    
 
 
  <Route 
     path='/register' 
     element={<Register 
-    //setToken={ setToken } 
-    //token={token} 
-    //navigate={navigate} 
+    setToken={ setToken } 
+    token={token} 
+    navigate={navigate} 
   />} 
 />
 <Route
     path='/login'
     element={ <Login 
-    //setToken={ setToken }
-    //navigate={ navigate }
+    setToken={ setToken }
+    navigate={ navigate }
   />}
 />
       </Routes>
     </BrowserRouter>
+    </div>
   );
 }
 
